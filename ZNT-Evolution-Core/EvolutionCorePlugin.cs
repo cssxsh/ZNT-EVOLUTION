baecdee47fd96d9b1a2fe5756c3e4a14f005876f
cssxsh <cssxsh@gmail.com>
2023-09-30 01:26:22 +0800

    rename: ZNT-Evolution

---
+++ ZNT-Evolution-Core/EvolutionCorePlugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace ZNT.Evolution.Core
+{
+    [BepInPlugin("xyz.cssxsh.znt.evolution.core", "Evolution Core", "0.1.0")]
+    public class EvolutionCorePlugin : BaseUnityPlugin
+    {
+        public void Start()
+        {
+            new Harmony(Info.Metadata.GUID).PatchAll();
+        }
+    }
+}
