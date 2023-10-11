b430415e31cd76dce38c87c2444701174830dcb7
cssxsh <cssxsh@gmail.com>
2023-10-12 03:26:22 +0800

    feat: CreateSingleSprite

--- ZNT-Evolution-Core/LevelElementLoader.cs
+++ ZNT-Evolution-Core/LevelElementLoader.cs
@@ -11,9 +11,8 @@ namespace ZNT.Evolution.Core
     {
         private static readonly ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource("LevelElementLoader");
 
-        public static LevelElement LoadFormFolder(string path)
+        public static LevelElement LoadFormFolder(string path, LevelElement.Type type)
         {
-            
             if (Directory.Exists(path))
             {
                 Logger.LogInfo($"load form folder '{path}'.");
@@ -23,12 +22,26 @@ namespace ZNT.Evolution.Core
                 Logger.LogInfo($"folder {path} not exists.");
                 return null;
             }
-            var bundle = LoadFormAssetBundle(path: Path.Combine(path, "resources.bundle"));
+
+            switch (type)
+            {
+                case LevelElement.Type.Brush:
+                    return LoadBrushFormFolder(path: path);
+                case LevelElement.Type.Decor:
+                    return LoadDecorFormFolder(path: path);
+            }
+
+            return null;
+        }
+
+        private static LevelElement LoadBrushFormFolder(string path)
+        {
+            var bundle = LoadAssetBundle(path: Path.Combine(path, "resources.bundle"));
             Logger.LogDebug($"resources.bundle -> {bundle} -> {bundle.GetAllAssetNames().Join()}");
 
             var sprites = CustomAssetUtility
                 .LoadComponentFromPath<tk2dSpriteCollectionData>(source: Path.Combine(path, "sprites.json"));
-            Logger.LogDebug($"sprites.json -> {sprites}");
+            Logger.LogDebug($"sprites.json -> {sprites} -> ${sprites.materials[0]}");
 
             var animation = CustomAssetUtility
                 .LoadComponentFromPath<tk2dSpriteAnimation>(source: Path.Combine(path, "animation.json"));
@@ -40,13 +53,40 @@ namespace ZNT.Evolution.Core
 
             var element = CustomAssetUtility
                 .DeserializeAssetFromPath<LevelElement>(source: Path.Combine(path, "element.json"));
-            Logger.LogDebug($"element.json -> {element}");
+            Logger.LogDebug($"element.json -> {element} -> ${element.Title}");
             AssetElementBinder.PushToIndex(element);
 
             return element;
         }
 
-        private static AssetBundle LoadFormAssetBundle(string path)
+        private static LevelElement LoadDecorFormFolder(string path)
+        {
+            var bundle = LoadAssetBundle(path: Path.Combine(path, "resources.bundle"));
+            Logger.LogDebug($"resources.bundle -> {bundle} -> {bundle.GetAllAssetNames().Join()}");
+
+            if (File.Exists(Path.Combine(path, "sprites.json")))
+            {
+                var sprites = CustomAssetUtility
+                    .LoadComponentFromPath<tk2dSpriteCollectionData>(source: Path.Combine(path, "sprites.json"));
+                Logger.LogDebug($"sprites.json -> {sprites} -> {sprites.materials[0]}");
+            }
+            else
+            {
+                var material = bundle.LoadAsset<Material>("sprites");
+                var sprites = CreateSingleSprite(material);
+                Logger.LogDebug($"create -> {sprites} -> {sprites.materials[0]}");
+                // CustomAssetUtility.SaveComponentToPath(target: Path.Combine(path, "sprites.json"), sprites);
+            }
+
+            var element = CustomAssetUtility
+                .DeserializeAssetFromPath<LevelElement>(source: Path.Combine(path, "element.json"));
+            Logger.LogDebug($"element.json -> {element} -> {element.Title}");
+            AssetElementBinder.PushToIndex(element);
+
+            return element;
+        }
+
+        private static AssetBundle LoadAssetBundle(string path)
         {
             AssetBundle bundle;
             {
@@ -61,44 +101,65 @@ namespace ZNT.Evolution.Core
 
             foreach (var name in bundle.GetAllAssetNames())
             {
-                Logger.LogDebug($"[AssetBundle] load path: {name}");
+                Logger.LogDebug($"[{bundle.name}] load path: {name}");
                 var request = bundle.LoadAssetAsync(name);
                 while (!request.isDone)
                 {
                     Thread.Sleep(100);
                 }
 
-                if (request.asset == null) continue;
+                if (request.asset == null)
+                {
+                    Logger.LogWarning("null !!!");
+                    continue;
+                }
 
                 request.asset.hideFlags = HideFlags.HideAndDontSave;
 
                 switch (request.asset)
                 {
-                    case null:
-                        Logger.LogWarning("null !!!");
-                        break;
                     case Sprite sprite:
-                        Logger.LogDebug(sprite.name);
-                        Logger.LogDebug(sprite.texture);
+                        Logger.LogDebug($"[{bundle.name}] {sprite}");
+                        Logger.LogDebug($"[{bundle.name}] {sprite.texture}");
                         break;
                     case Material material:
-                        Logger.LogDebug(material.name);
-                        Logger.LogDebug(material.mainTexture);
-                        Logger.LogDebug(material.shader.name);
+                        Logger.LogDebug($"[{bundle.name}] {material}");
+                        Logger.LogDebug($"[{bundle.name}] {material.mainTexture}");
+                        Logger.LogDebug($"[{bundle.name}] {material.shader}");
                         break;
                     case Shader shader:
-                        Logger.LogDebug(shader.name);
+                        Logger.LogDebug($"[{bundle.name}] {shader}");
                         break;
                     case Rotorz.Tile.OrientedBrush brush:
-                        Logger.LogDebug(brush.name);
+                        Logger.LogDebug($"[{bundle.name}] {brush}");
                         break;
                     default:
-                        Logger.LogDebug(request.asset.name);
+                        Logger.LogDebug($"[{bundle.name}] {request.asset.name}");
                         break;
                 }
             }
 
             return bundle;
+        }
+
+        private static tk2dSpriteCollectionData CreateSingleSprite(Material material)
+        {
+            var impl = tk2dSpriteCollectionData.CreateFromTexture(
+                texture: material.mainTexture,
+                size: tk2dSpriteCollectionSize.Explicit(0.5F, 12),
+                names: new []{ "???" },
+                regions: new []{ new Rect(0, 0, material.mainTexture.width, material.mainTexture.height) },
+                anchors: new []{ Vector2.zero }
+            );
+
+            impl.name = material.name.Replace("_mat", "");
+            impl.gameObject.hideFlags = HideFlags.HideAndDontSave;
+            impl.material = material;
+            impl.materials[0] = material;
+            impl.spriteDefinitions[0].material = material;
+            impl.spriteDefinitions[0].name = impl.name.Replace("sprites_", "");
+
+            return impl;
         }
     }
 }
