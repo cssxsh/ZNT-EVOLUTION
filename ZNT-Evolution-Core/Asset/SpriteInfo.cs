e73ffe1b7b8b64b2ba3e9f7514c0bff3010d693b
cssxsh <cssxsh@gmail.com>
2023-10-21 04:07:00 +0800

    feat: single anchor

--- ZNT-Evolution-Core/Asset/SpriteInfo.cs
+++ ZNT-Evolution-Core/Asset/SpriteInfo.cs
@@ -15,17 +15,20 @@ namespace ZNT.Evolution.Core.Asset
         [JsonProperty("Names")] public readonly string[] Names;
 
         [JsonProperty("Regions")] public readonly Rect[] Regions;
-        
+
         [JsonProperty("Anchors")] public readonly Vector2[] Anchors;
 
         [JsonConstructor]
-        public SpriteInfo(float orthoSize, float targetHeight, string[] names, Rect[] regions, Vector2[] anchors)
+        public SpriteInfo(
+            float orthoSize, float targetHeight,
+            string[] names, Rect[] regions, Vector2[] anchors = null,
+            Vector2? anchor = null)
         {
             OrthoSize = orthoSize;
             TargetHeight = targetHeight;
             Names = names;
             Regions = regions;
-            Anchors = anchors ?? Regions.Select(_ => Vector2.zero).ToArray();
+            Anchors = anchors ?? Regions.Select(_ => anchor ?? Vector2.zero).ToArray();
         }
     }
 }
