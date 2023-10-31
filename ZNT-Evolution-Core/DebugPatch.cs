61b73c41ec2141d2ee65c03bda862bcc98390974
cssxsh <cssxsh@gmail.com>
2023-10-31 19:44:59 +0800

    style: debug log

--- ZNT-Evolution-Core/DebugPatch.cs
+++ ZNT-Evolution-Core/DebugPatch.cs
@@ -6,19 +6,19 @@ namespace ZNT.Evolution.Core
 {
     public static class DebugPatch
     {
-        [HarmonyPatch(typeof(SceneLoader), "LoadNextScene"), HarmonyPrefix]
+        [HarmonyPatch(typeof(SceneLoader), methodName: "LoadNextScene"), HarmonyPrefix]
         public static void LoadNextScene(ref string sceneName)
         {
             BepInEx.Logging.Logger.CreateLogSource("SceneLoader").LogInfo("LoadNextScene: " + sceneName);
         }
 
-        [HarmonyPatch(typeof(CharacterAsset), "LoadFromAsset"), HarmonyPrefix]
-        public static void LoadFromAsset(CharacterAsset __instance, GameObject gameObject)
+        [HarmonyPatch(typeof(CustomAssetObject), methodName: "LoadFromAsset"), HarmonyPrefix]
+        public static void LoadFromAsset(CustomAssetObject __instance, GameObject gameObject)
         {
-            BepInEx.Logging.Logger.CreateLogSource("CharacterAsset").LogInfo("LoadFromAsset: " + gameObject.name);
+            BepInEx.Logging.Logger.CreateLogSource("CustomAssetObject").LogInfo("LoadFromAsset: " + gameObject.name);
         }
 
-        [HarmonyPatch(typeof(Material), "GetTexture", typeof(string)), HarmonyPrefix]
+        [HarmonyPatch(typeof(Material), methodName: "GetTexture", argumentTypes: typeof(string)), HarmonyPrefix]
         public static bool GetTexture(Material __instance, string name) => __instance.HasProperty(name);
     }
 }
