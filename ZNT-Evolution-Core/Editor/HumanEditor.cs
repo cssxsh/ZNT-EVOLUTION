e300b79959e9b3c3faa9f65c3429fe4fa296ee3f
cssxsh <cssxsh@gmail.com>
2025-08-17 19:56:36 +0800

    feat: HumanEditor

--- ZNT-Evolution-Core/Editor/HumanEditor.cs
+++ ZNT-Evolution-Core/Editor/HumanEditor.cs
@@ -7,7 +7,42 @@ namespace ZNT.Evolution.Core.Editor
     public class HumanEditor : EditorComponent
     {
         private HumanBehaviour Behaviour => GetComponentInChildren<HumanBehaviour>();
-        
+
+        [SerializeInEditor(name: "Flee Before Zombie Explode")]
+        public bool FleeBeforeZombieExplode
+        {
+            get => Behaviour.FleeBeforeZombieExplode;
+            set => Behaviour.FleeBeforeZombieExplode = value;
+        }
+
+        [SerializeInEditor(name: "Flee Duration")]
+        public float FleeDuration
+        {
+            get => Behaviour.FleeingTimer.Duration;
+            set => Behaviour.FleeingTimer.Duration = value;
+        }
+
+        [SerializeInEditor(name: "Direct Aim")]
+        public bool DirectAim
+        {
+            get => Behaviour.Attacker.DirectAim;
+            set => Behaviour.Attacker.DirectAim = value;
+        }
+
+        [SerializeInEditor(name: "Aim Stop Time")]
+        public float AimStopTime
+        {
+            get => Behaviour.Attacker.StopAimingTime;
+            set => Behaviour.Attacker.StopAimingTime = value;
+        }
+
+        [SerializeInEditor(name: "Aim Range")]
+        public float AimRange
+        {
+            get => Behaviour.Attacker.AimRange;
+            set => Behaviour.Attacker.AimRange = value;
+        }
+
         [SerializeInEditor(name: "Voice")]
         public Voice Voice
         {
