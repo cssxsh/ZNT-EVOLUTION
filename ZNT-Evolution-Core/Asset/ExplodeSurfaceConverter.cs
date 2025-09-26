24de967cc50d71e650e7c1a5a9f7b5bf397fe61a
cssxsh <cssxsh@gmail.com>
2025-09-26 16:52:56 +0800

    fix: ExplodeSurfaceConverter

--- ZNT-Evolution-Core/Asset/ExplodeSurfaceConverter.cs
+++ ZNT-Evolution-Core/Asset/ExplodeSurfaceConverter.cs
@@ -4,36 +4,37 @@ using System.Linq;
 using HarmonyLib;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using ExplodeSurface = PhysicObjectBehaviour.ExplodeSurface;
 
 namespace ZNT.Evolution.Core.Asset
 {
-    internal class ExplodeSurfaceConverter : CustomCreationConverter<PhysicObjectBehaviour.ExplodeSurface>
+    internal class ExplodeSurfaceConverter : CustomCreationConverter<ExplodeSurface>
     {
-        public const PhysicObjectBehaviour.ExplodeSurface None = 0x00000000;
+        public const ExplodeSurface None = 0x00000000;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Wall = PhysicObjectBehaviour.ExplodeSurface.Wall;
+        public const ExplodeSurface Wall = ExplodeSurface.Wall;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Ground = PhysicObjectBehaviour.ExplodeSurface.Ground;
+        public const ExplodeSurface Ground = ExplodeSurface.Ground;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Ceiling = PhysicObjectBehaviour.ExplodeSurface.Ceiling;
+        public const ExplodeSurface Ceiling = ExplodeSurface.Ceiling;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Target = PhysicObjectBehaviour.ExplodeSurface.Target;
+        public const ExplodeSurface Target = ExplodeSurface.Target;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Zombie = (PhysicObjectBehaviour.ExplodeSurface)0x00000010;
+        public const ExplodeSurface Zombie = (ExplodeSurface)0x00000010;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Climber = (PhysicObjectBehaviour.ExplodeSurface)0x00000020;
+        public const ExplodeSurface Climber = (ExplodeSurface)0x00000020;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Blocker = (PhysicObjectBehaviour.ExplodeSurface)0x00000040;
+        public const ExplodeSurface Blocker = (ExplodeSurface)0x00000040;
 
-        public const PhysicObjectBehaviour.ExplodeSurface Tank = (PhysicObjectBehaviour.ExplodeSurface)0x00000080;
+        public const ExplodeSurface Tank = (ExplodeSurface)0x00000080;
 
-        public const PhysicObjectBehaviour.ExplodeSurface IgnoreHuman = Zombie | Climber | Blocker | Tank;
+        public const ExplodeSurface IgnoreHuman = Zombie | Climber | Blocker | Tank;
 
         public override bool CanWrite => true;
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            var mask = (PhysicObjectBehaviour.ExplodeSurface)value;
+            var mask = (ExplodeSurface)value;
             var flags = new List<string>();
             if (mask.HasFlag(Wall)) flags.Add(nameof(Wall));
             if (mask.HasFlag(Ground)) flags.Add(nameof(Ground));
@@ -46,12 +47,13 @@ namespace ZNT.Evolution.Core.Asset
             writer.WriteValue(flags.Join());
         }
 
-        public override PhysicObjectBehaviour.ExplodeSurface Create(Type type) => None;
-
         public override bool CanRead => true;
+
+        public override ExplodeSurface Create(Type type) => None;
 
         public override object ReadJson(JsonReader reader, Type type, object _, JsonSerializer serializer)
         {
+            if (reader.TokenType == JsonToken.Integer) return (ExplodeSurface)serializer.Deserialize<int>(reader);
             var flags = serializer.Deserialize<string>(reader).Split(',', ' ');
             return flags.Aggregate(None, (mask, flag) => mask | flag switch
             {
