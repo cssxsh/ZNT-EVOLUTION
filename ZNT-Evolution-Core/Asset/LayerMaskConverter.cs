921cf2ab645219c3f64725d1e247511ab1149dd2
cssxsh <cssxsh@gmail.com>
2026-08-03 16:40:00 +0800

    fix: LayerMaskConverter

--- ZNT-Evolution-Core/Asset/LayerMaskConverter.cs
+++ ZNT-Evolution-Core/Asset/LayerMaskConverter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Linq;
+using System.Text.RegularExpressions;
 using BepInEx.Logging;
 using HarmonyLib;
 using Newtonsoft.Json;
@@ -27,6 +28,7 @@ public class LayerMaskConverter : CustomCreationConverter<LayerMask>
 
         var names =
             from index in Enumerable.Range(0x00, 0x20)
+            where (mask.value | (0x01 << index)) == mask.value
             let name = LayerMask.LayerToName(index)
             select string.IsNullOrEmpty(name) ? index.ToString() : name;
 
@@ -41,12 +43,28 @@ public class LayerMaskConverter : CustomCreationConverter<LayerMask>
     {
         if (reader.TokenType == JsonToken.Integer) return (LayerMask)serializer.Deserialize<int>(reader);
         if (reader.TokenType != JsonToken.String) return JToken.Load(reader).ToObject<LayerMask>();
-        var names = serializer.Deserialize<string>(reader).Split(',');
-        return (LayerMask)names.Select(n => n.Trim()).Aggregate(0x00000000, (mask, name) =>
+        var value = serializer.Deserialize<string>(reader);
+        var mask = 0x00000000;
+        foreach (var match in Regex.Matches(value, """(\w(?:[\s\w]*\w)?)""").Cast<Match>())
         {
-            var layer = LayerMask.NameToLayer(name);
-            if (layer == -1 && !int.TryParse(name, out layer)) Logger.LogError($"Invalid Layer '{name}'");
-            return mask | (0x01 << layer);
-        });
+            var layer = LayerMask.NameToLayer(match.Value);
+            if (layer is not -1 || int.TryParse(match.Value, out layer))
+            {
+                mask |= 0x01 << layer;
+                continue;
+            }
+
+            // SplitCamelCase
+            layer = LayerMask.NameToLayer(Regex.Replace(match.Value, "(?:[a-z])([A-Z])", " $1"));
+            if (layer is not -1)
+            {
+                mask |= 0x01 << layer;
+                continue;
+            }
+
+            Logger.LogError($"Invalid Layer '{match.Value}'");
+        }
+
+        return (LayerMask)mask;
     }
 }
