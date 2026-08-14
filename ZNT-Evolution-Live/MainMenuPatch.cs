b408445db64c30fa184c526021e82f5feb7842f6
cssxsh <cssxsh@gmail.com>
2026-08-14 10:35:36 +0800

    remove: Live Button

--- ZNT-Evolution-Live/MainMenuPatch.cs
+++ ZNT-Evolution-Live/MainMenuPatch.cs
@@ -8,32 +8,6 @@ namespace ZNT.Evolution.Live;
 internal static class MainMenuPatch
 {
     [HarmonyPostfix]
-    [HarmonyPatch(typeof(MainMenu), "Start")]
-    public static void Start(MainMenu __instance)
-    {
-        var canvas = __instance.transform.Find("Canvas");
-        var live = Object.Instantiate(original: canvas.Find("Stats Button"), parent: canvas);
-        {
-            live.name = "Live Button";
-            live.transform.localPosition = new Vector3(-77.0f, -208.0f, 0.0f);
-            var tooltip = live.GetComponent<TooltipReceiver>();
-            Traverse.Create(tooltip).Field<string>("text").Value = LiveManager.Instance.LiveState.Term;
-            var button = live.GetComponent<Button>();
-            button.onClick = new Button.ButtonClickedEvent();
-            button.onClick.AddListener(LiveManager.Instance.ToggleActivation);
-            var image = live.GetComponent<Image>();
-            image.enabled = false;
-        }
-    }
-
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(MainMenu), "Quit")]
-    public static void Quit(MainMenu __instance)
-    {
-        LiveManager.Instance.SetInactive();
-    }
-
-    [HarmonyPostfix]
     [HarmonyPatch(typeof(ZNT.LevelEditor.MainMenu), "OnCreate")]
     public static void OnCreate(ZNT.LevelEditor.MainMenu __instance)
     {
