d8cbb48e1f42f48c2410e9195c634d2bb4dbd263
cssxsh <cssxsh@gmail.com>
2026-08-28 14:09:59 +0800

    remove: DamageDictionary

--- ZNT-Evolution-Core/Asset/DamageDictionary.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ZNT.Evolution.Core.Asset;
-
-[Serializable]
-public class DamageDictionary<T> : UnityDictionary<DamageType, T>
-{
-    protected override void OnDeserialize() => SetComparer(DamageTypeComparer.Instance);
-}
